cba8d0926134c1ffca11539f50c13f35b8c3e2a6
davidddeveloper <programmingdonebydavid@gmail.com>
2025-12-25 13:55:05 +0000

    reder a proper client component

---
+++ app/manifesto/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Manifesto() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">Manifesto</h1>
+        </div>
+    )
+}
---
+++ app/ventures/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Ventures() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">Ventures</h1>
+        </div>
+    )
+}
